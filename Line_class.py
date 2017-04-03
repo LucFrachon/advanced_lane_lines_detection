debug: Fix unit conversion issue

Line_class.py
@@ -38,12 +38,14 @@ class Queue():
 
 
 class Line():
-    def __init__(self, color, img_size, last_n = 5, search_n = 10, 
+    def __init__(self, color, img_size, pxm_ratio, last_n = 5, search_n = 10, 
         pos_tol = 0.2, rad_tol = 0.2):
         '''
         color:      color in which to display the line
-        order:      polynomial order for fitted lines
+        img_size:   Size of the images we're working with
+        pxm_ratio:  Conversion ratio from pixels to meters
         last_n:     int, how many previous fits should be remembered?
+        search_n:   int, how many failed fits before doing a full window search?
         pos_tol:    tolerance on the order 0 coefficient for a fit to be 
                     considered valid (as a proportion of the mean of the n last
                     good fits)
@@ -52,6 +54,9 @@ class Line():
         '''
         # Image size:
         self.img_size = img_size
+
+        # Pixels to meters ratios:
+        self.pxm_x, self.pxm_y = pxm_ratio
 
         # Number of sets of fit coefficients to remember:
         self.last_n = last_n
@@ -84,14 +89,11 @@ class Line():
         #radius of curvature of the last n iterations
         self.last_n_radius = Queue(1)
 
-        #radius of curvature of the line in some units
+        #radius of curvature of the line in pixels
         self.current_curvature = None
 
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = self.img_size[0] / 2.
-
-        #difference in fit coefficients between last and new fits
-        #self.diffs = np.array([0,0,0], dtype='float')
+        # Estimated position of the line at the bottom of the screen
+        self.line_base_pos = None
 
         #x values for detected line pixels (current frame)
         self.all_x = None  
@@ -104,7 +106,10 @@ class Line():
 
 
     def compute_curvature(self, fit, y):
-
+        '''
+        fit:    Polynomial coefficients, calculated in real-world coordinates
+        y:      Positions along the y axis, in real-world coordinates 
+        '''
         y_eval = np.max(y)
         radius = np.asscalar(((1 + (2 * fit[0] * y_eval + \
             fit[1]) ** 2) ** 1.5) / np.absolute(2 * fit[0] + 0.0001))
@@ -113,14 +118,15 @@ class Line():
 
     def update_line(self, fit, curve_rad):
         '''
-        Once a valid line has been found and fitted, this function updates the 
+        Once a valid line has been found and fitted, this method updates the 
         line attibutes.
         '''
-
         self.detected = True
         self.last_n_fits.enqueue(fit)
         self.last_n_radius.enqueue(np.array(curve_rad)[np.newaxis])
-        self.line_base_pos = self.base_line_position()
+        self.line_base_pos = self.base_line_position(fit)
+        # print(len(self.last_n_fits.items), self.last_n_fits.items[0])
+
         # Don't let the queue exceed the maximum length:
         if len(self.last_n_fits.items) > self.last_n:
             self.last_n_fits.dequeue()
@@ -133,13 +139,16 @@ class Line():
         Compare coefficients from the latest polynomial fit and the x position of 
         the line to the average n latest good fits. Return True if they pass
         the validation condition.
+
+        Polynomial coefficients are calculated using real-world coordinates.
         '''
         
         fits_empty = self.last_n_fits.is_empty()  # Is the queue of coefs empty?
         radius_empty = self.last_n_radius.is_empty()  # Is the queue of radiuses empty?
 
-        if (fits_empty | radius_empty) | \
-        (self.frames_since_detection >= self.last_n):
+        if (fits_empty | radius_empty) \
+            | (self.frames_since_detection >= self.last_n):
+            # print("Queues empty or Too long since detection")
             return True  # If either queue is empty or we haven't been able to 
             # validate a fit since more than last_n frames, we accept these 
             # coefficients even thought we weren't able to make sure they made
@@ -150,25 +159,36 @@ class Line():
             last_n_rad_mean = self.last_n_radius.mean()
             # We want the base line position to be within tolerance of the latest
             # n average:
-            max_y = self.img_size[1] - 1
-            current_pos = self.current_fit[0] * max_y ** 2 + \
-                self.current_fit[1] * max_y + self.current_fit[2]
-            avg_pos = last_n_fits_mean[0] * max_y ** 2 + \
-                last_n_fits_mean[1] * max_y + last_n_fits_mean[2]
+            # Convert the maximum y to real-world coordinates:
+            max_y = (self.img_size[1] - 1) * self.pxm_y
 
+            current_pos = self.base_line_position(self.current_fit)
+            avg_pos = self.base_line_position(last_n_fits_mean)
+
+            # current_pos = self.current_fit[0] * max_y ** 2 + \
+            #     self.current_fit[1] * max_y + self.current_fit[2]
+            # avg_pos = last_n_fits_mean[0] * max_y ** 2 + \
+            #     last_n_fits_mean[1] * max_y + last_n_fits_mean[2]
+            # print("Current x=", current_pos, "Avg x=", avg_pos)
             position_ok = np.absolute((current_pos - avg_pos) / avg_pos) \
                 <= self.pos_tol
-            radius_ok = np.absolute((self.current_curvature - \
-                last_n_rad_mean) / last_n_rad_mean) <= self.rad_tol
-        
+            # print("Position OK?", position_ok)
+            # print("Current Radius=", self.current_curvature, "Avg Radius=", last_n_rad_mean)
+            radius_ok = np.squeeze(np.absolute((self.current_curvature - \
+                last_n_rad_mean) / last_n_rad_mean) <= self.rad_tol)
+            # print("Radius OK?", radius_ok)
         return (position_ok & radius_ok)
 
 
     def fit_poly(self, y):
         '''
-        Fit a 2nd-order polynomial to the found x and y pixel coordinates and 
+        Fit a 2nd-order polynomial to the found x and y pixel coordinates (in 
+        image space), convert to real-world coordinates and  
         update the line's coefficient queue if the coefficients pass the sanity 
         check.
+
+        - y: Range of y coordinates in image-space coordinates
+
         Returns:
         - valid:    Boolean, indicates successful fit with valid coefficients
         - self.current_fit: The fitted coefficients
@@ -176,10 +196,12 @@ class Line():
         
         # First make sure we actually have points to fit:
         xy_ok = (self.all_x.shape != (0,)) & (self.all_y.shape != (0,))
-
-        if xy_ok: # If so, fit polynomial and get coefficients
-            self.current_fit = np.polyfit(self.all_y, self.all_x, 2)
-            self.current_curvature = self.compute_curvature(self.current_fit, y)
+        
+        if xy_ok: # If so, convert coordinates and fit polynomial:
+            self.current_fit = np.polyfit(self.all_y * self.pxm_y,
+             self.all_x * self.pxm_x, 2)
+            self.current_curvature = self.compute_curvature(self.current_fit, 
+                y * self.pxm_y)
 
             if self.sanity_check():  # If coefficients seem sensible:
                 self.update_line(self.current_fit, self.current_curvature)
@@ -201,11 +223,17 @@ class Line():
         Calculates the outcome values associated to each y for the latest valid
         polynomial coefficients. Warning: y refers to the image coordinates, 
         therefore it is the predictor here. x is the outcome.
-        Returns self.last_x_fitted, an array containing the calculated x values.
+
+        - y: Range of y values in image-space coordinates
+
+        Returns:    self.last_x_fitted, an array containing the calculated x values 
+                    in real-world coordinates.
 
         '''
         fit = self.last_n_fits.items[0]
-        self.last_x_fitted = fit[0] * y**2 + fit[1] * y + fit[2]
+        # Convert to real-world coordinates and calculate resulting values:
+        self.last_x_fitted = fit[0] * (y * self.pxm_y)**2 + \
+            fit[1] * y * self.pxm_y + fit[2]
 
         return self.last_x_fitted
 
@@ -216,27 +244,30 @@ class Line():
         n latest polynomial coefficients. 
         Warning: y refers to the image coordinates, therefore it is the predictor 
         here. x is the outcome.
-        Returns self.last_x_fitted, an array containing the calculated x values.
+        y is given in image-space coordinates.
+
+        Returns self.last_x_fitted, an array containing the calculated x values 
+        in real-world coordinates.
 
         '''
 
         fit = [self.last_n_fits.mean()[0], self.last_n_fits.mean()[1], 
             self.last_n_fits.mean()[2]]
         self.last_x_fitted = np.maximum(0, 
-            np.minimum(fit[0] * y**2 + fit[1] * y + fit[2], 
-            self.img_size[0] - 1))
+            np.minimum(fit[0] * (y * self.pxm_y) **2 + \
+                fit[1] * y * self.pxm_y + fit[2], 
+                (self.img_size[0] - 1) * self.pxm_x))
 
         return self.last_x_fitted
 
-    def base_line_position(self):
+    def base_line_position(self, fit):
         '''
         Computes the position of the line at the bottom of the image, in relation 
         to the left border, based on the latest valid line fit.
+        The result is expressed in real-world coordinates.
         '''
 
-        fit = self.last_n_fits.items[0]
-
-        position = fit[0] * (self.img_size[1] - 1)**2 + \
-            fit[1] * (self.img_size[1] - 1) + fit[2]
-
+        position = fit[0] * ((self.img_size[1] - 1) * self.pxm_y)**2 + \
+            fit[1] * (self.img_size[1] - 1) * self.pxm_y + fit[2]
+        # print("Base Line Position", position)
         return position